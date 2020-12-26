940a8083d4eac9826952bda7819f300461702453
Paolo Veneruso Rodriguez <pvenrod@gmail.com>
2020-12-26 10:18:55 +0100

    Commit inicial

---
+++ app/Http/Controllers/HolaController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HolaController extends Controller
+{
+    public function index() {
+        return "Hola, mundo";
+    }
+}
